34493897000a7b8c9d5dc53c152d7821b989bfdf
Akanimomichael <akanimomichael@gmail.com>
2022-06-02 14:55:16 -0700

    mandatory task

--- 0x00-hello_world/5-printf.c
+++ 0x00-hello_world/5-printf.c
@@ -2,10 +2,11 @@
 
 
 /**
-* main - prints the phrase with proper grammar,
-* but the outcome is a piece of art,
-* Return: 0 if exited properly, non-zero otherwise
+ * main - prints the phrase with proper grammar,
+ * but the outcome is a piece of art,
+ * Return: 0 if exited properly, non-zero otherwise
 */
+
 int main(void)
 {
 printf("with proper grammar, but the outcome is a piece of art,\n");
