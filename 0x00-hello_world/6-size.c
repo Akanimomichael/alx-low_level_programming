39f4a991f4e4e8e98ad022b4dd22b104fe5d4ed5
Akanimomichael <akanimomichael@gmail.com>
2022-06-03 04:03:07 -0700

    mandatory task

--- 0x00-hello_world/6-size.c
+++ 0x00-hello_world/6-size.c
@@ -2,12 +2,9 @@
 
 
 /**
- *
- *  * main - prints the size of various types
- *
- *   * Return: 0 if exited properly, non-zero otherwise
- *
- *    */
+* main - prints the size of various types
+* Return: 0 if exited properly, non-zero otherwise
+*/
 
 int main(void)
 
