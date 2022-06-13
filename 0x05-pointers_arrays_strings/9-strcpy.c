e4e8d803ae7fe6e5bf46fc60e2d5b654557d54d8
Akanimomichael <101297309+Akanimomichael@users.noreply.github.com>
2022-06-13 22:32:17 +0100

    Update 9-strcpy.c

--- 0x05-pointers_arrays_strings/9-strcpy.c
+++ 0x05-pointers_arrays_strings/9-strcpy.c
@@ -1,24 +1,21 @@
 #include "main.h"
-#include <stdio.h>
+
 /**
-* print_array - print `n` elements of an array of integers
-* @a: int type array pointer
-* @n: int type integer
-* Description: Numbers must be separated by comma and space.
-* Numbers should be displayed in the same order they are stored in array.
-* You can only use _putchar to print.
-*/
-void print_array(int *a, int n)
+ *_strcpy - copy stringat the end of another string including \0.
+ *@dest: pointer to string to be copied to.
+ *@src: pointer to string to be copied.
+ *
+ *Return: pointer to dest.
+ */
+
+char *_strcpy(char *dest, char *src)
 {
-int i;
-i = 0;
-for (n--; n >= 0; n--, i++)
-{
-printf("%d", a[i]);
-if (n > 0)
-{
-printf(", ");
-}
-}
-printf("\n");
+	int j;
+
+	for (j = 0; src[j] != '\0'; j++)
+	{
+		dest[j] = src[j];
+	}
+	dest[j] = '\0';
+	return (dest);
 }
