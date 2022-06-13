092936f18579c57cecd852ebbb787bc130ff2e5d
Akanimomichael <101297309+Akanimomichael@users.noreply.github.com>
2022-06-13 22:08:21 +0100

    Cool

--- 0x05-pointers_arrays_strings/3-puts.c
+++ 0x05-pointers_arrays_strings/3-puts.c
@@ -1,18 +1,18 @@
-#include "holberton.h"
-/**
- * _puts - prints a string, followed by a new line,
- * @str: pointer to the string to be printed
- * Return: void
-*/
+#include "main.h"
 
+/**
+ * _puts - prints a string and a new line
+ * @str: pointer to a string to print
+ * Return: 0
+ */
 
 void _puts(char *str)
 {
 int i = 0;
 while (str[i])
 {
-	_putchar(str[i]);
-	i++;
+_putchar(str[i]);
+i++;
 }
 _putchar('\n');
 }
