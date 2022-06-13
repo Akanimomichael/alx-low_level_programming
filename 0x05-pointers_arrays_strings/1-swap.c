b2fe88d31c27030dd05762f8536f13dc95951bc2
Akanimomichael <akanimomichael@gmail.com>
2022-06-13 10:30:00 -0700

    1st commit

---
+++ 0x05-pointers_arrays_strings/1-swap.c
@@ -0,0 +1,13 @@
+nclude "main.h"
+/**
+* swap_int - swaps the value of two integers
+* @a: is first integer
+* @b: is second integer
+*/
+void swap_int(int *a, int *b)
+{
+int c = *a;
+int d = *b;
+*a = d;
+*b = c;
+}
